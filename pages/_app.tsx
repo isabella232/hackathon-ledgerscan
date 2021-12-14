258550cdeb2f2f1bacc9d797167a0b2677876515
Thomas Laforge <thomas.laforge@ledger.fr>
2021-12-14 10:10:44 +0100

    load ui library from app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useState } from 'react'
+import "@ledgerhq/react-ui/assets/fonts"; // all fonts are consumed by the bundler and outputted to /assets
+import { StyleProvider } from "@ledgerhq/react-ui";
+
+type Palette = "light" | "dark" | undefined;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [palette, setPalette] = useState("light" as Palette);
+  return (
+    <StyleProvider fontsPath="assets" selectedPalette={palette}>
+      <Component {...pageProps} />
+    </StyleProvider>
+  )
 }
 
 export default MyApp
