e0c3cd56f8409111d496045a8c60edf73b1ce0cc
Thomas Laforge <thomas.laforge@ledger.fr>
2021-12-14 18:30:21 +0100

    add styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,19 +2,26 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import "@ledgerhq/react-ui/assets/fonts"; // all fonts are consumed by the bundler and outputted to /assets
 import { StyleProvider } from "@ledgerhq/react-ui";
-import styles from '../styles/App.module.css'
+import styles from '../styles/App.module.css';
+import styled from 'styled-components'
+
+const App = styled.div`
+  background-color: ${(p) => p.theme.colors.background.main};
+`
 
 function RootStyle({ children } : { children: JSX.Element }){
   return <div className={styles.container}>
     <div className={styles.main}>
-      {children}
+      <App>
+        {children}
+      </App>
     </div>
   </div>
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <StyleProvider fontsPath="assets" selectedPalette={"dark"}>
+    <StyleProvider selectedPalette="dark">
       <RootStyle>
         <Component {...pageProps} />
       </RootStyle>
