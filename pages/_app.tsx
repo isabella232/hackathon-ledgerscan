c718fa81c5c92cb76fb462414db998c7fb73b472
Thomas Laforge <thomas.laforge@ledger.fr>
2021-12-14 14:53:29 +0100

    add root component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,22 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import "@ledgerhq/react-ui/assets/fonts"; // all fonts are consumed by the bundler and outputted to /assets
 import { StyleProvider } from "@ledgerhq/react-ui";
+import styles from '../styles/App.module.css'
+
+function RootStyle({ children } : { children: JSX.Element }){
+  return <div className={styles.container}>
+    <div className={styles.main}>
+      {children}
+    </div>
+  </div>
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <StyleProvider fontsPath="assets" selectedPalette={"dark"}>
-      <Component {...pageProps} />
+      <RootStyle>
+        <Component {...pageProps} />
+      </RootStyle>
     </StyleProvider>
   )
 }
