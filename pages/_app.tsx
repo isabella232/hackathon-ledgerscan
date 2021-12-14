d8604d1157e2152a9d0376a12fffb0879f1e1f31
Thomas Laforge <thomas.laforge@ledger.fr>
2021-12-14 13:59:33 +0100

    only dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { useState } from 'react'
 import "@ledgerhq/react-ui/assets/fonts"; // all fonts are consumed by the bundler and outputted to /assets
 import { StyleProvider } from "@ledgerhq/react-ui";
 
-type Palette = "light" | "dark" | undefined;
-
 function MyApp({ Component, pageProps }: AppProps) {
-  const [palette, setPalette] = useState("light" as Palette);
   return (
-    <StyleProvider fontsPath="assets" selectedPalette={palette}>
+    <StyleProvider fontsPath="assets" selectedPalette={"dark"}>
       <Component {...pageProps} />
     </StyleProvider>
   )
