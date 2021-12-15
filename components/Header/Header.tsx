01b5d151547720b339af0b9db5f29610adf13b63
Thomas Laforge <thomas.laforge@ledger.fr>
2021-12-15 11:16:07 +0100

    make logo as a link

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import {Header as LedgerHeader,Text, Log, Flex} from '@ledgerhq/react-ui';
 import styled from 'styled-components';
 import SplitSearch from '../SplitSearch/SplitSearch'
+import { useRouter } from 'next/router';
 
 const LedgerLog = styled(Log).attrs(() => ({
   width: "180px"
@@ -12,8 +13,13 @@ const ScanPart = styled(Text)`
   padding-left: 15px;
 ` 
 
-export const Logo = (): JSX.Element  => {
-  return <Flex flexDirection={"row"}>
+export const Logo = ({header}: {header: boolean}): JSX.Element  => {
+  const router = useRouter()
+
+  return <Flex flexDirection={"row"}
+    style={{ cursor: header ? "pointer" : "inherit" }}
+    onClick={() => header && router.push('/')}
+  >
     <LedgerLog>LEDGER</LedgerLog>
     <ScanPart>SCAN</ScanPart>
   </Flex>
@@ -34,7 +40,7 @@ export const Header = (): JSX.Element => {
   return (
     <HeaderZone>
       <HeaderContent
-        left={<Logo />} 
+        left={<Logo header={true} />} 
         right={<SplitSearch />} 
       />
     </HeaderZone>
